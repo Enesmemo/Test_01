c001891a84d8b6050d108069f60d547c8a6c2613
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-01-29 08:20:48 +0300

    Update EQQ.js

--- EQQ.js
+++ EQQ.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer (function(req, res) {
 res.writeHead (200, {'Content-type': 'text/plain'});
-res.end('Hello This first nodejs script!!');
+res.end('Bu Kalp Enes Adlı Şahısa Bırakılmıştır <3');
 }).listen(8000);
