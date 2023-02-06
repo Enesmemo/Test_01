c7faa934bf7afcbdf158c238004a329242b9486b
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-02-06 17:57:32 +0300

    Add files via upload

---
+++ Tahmin.py
@@ -0,0 +1,14 @@
+import random
+while True:
+    try:
+        Rand = random.randint(1,10)
+        Numb = int(input("Tahmin: "))
+        if Numb <= 10 and Numb > 0:
+            if Numb == Rand:
+                print("Bildin | Doğru Tahmin:", Rand)
+            else:
+                print("Bilemedin | Doğru Tahmin:", Rand)
+        else:
+            print("Hata: Tahminin 10 İla 1 Arasında Olmalı.")                    
+    except:
+        print("Hata: Yalnızca Sayı Yazınız.")                          
