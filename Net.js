132c2fad6f50714d4003cba89088e0735012ee13
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-02-06 17:56:44 +0300

    Update Net.js

--- Net.js
+++ Net.js
@@ -1,10 +1,1 @@
-var express = require("express");
-var app = express();
-app.get("/", function(req, res) {
-     res.send("Merhabalar, bu bir express ornegidir.");
-});
-app.get("/test", function(req, res) {
-     res.send("Bu bir test denemesidir.");
-});
-var server = app.listen(3000, function(){ console.log("Sunucu su anda port numarasinda : %d", server.address().port );
-});
+
