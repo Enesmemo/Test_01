cdec15eafc16ab0769bd78977eb4cf9a77186fce
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-02-07 12:59:50 +0300

    Update Tahmin.py

--- Tahmin.py
+++ Tahmin.py
@@ -3,6 +3,9 @@ while True:
     try:
         Rand = random.randint(1,10)
         Numb = int(input("Tahmin: "))
+        if Numb == 99:
+            print("Programdan Çıkış Sağlanıyor...")
+            break
         if Numb <= 10 and Numb > 0:
             if Numb == Rand:
                 print("Bildin | Doğru Tahmin:", Rand)
