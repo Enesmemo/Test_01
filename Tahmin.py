772e90e77b1cbcd8279c50336efa6a8ced422ed5
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-04-15 07:26:36 +0300

    Deneme

--- Tahmin.py
+++ Tahmin.py
@@ -1,17 +1,13 @@
-import random
-while True:
-    try:
-        Rand = random.randint(1,10)
-        Numb = int(input("Tahmin: "))
-        if Numb == 99:
-            print("Programdan Çıkış Sağlanıyor...")
-            break
-        if Numb <= 10 and Numb > 0:
-            if Numb == Rand:
-                print("Bildin | Doğru Tahmin:", Rand)
-            else:
-                print("Bilemedin | Doğru Tahmin:", Rand)
-        else:
-            print("Hata: Tahminin 10 İla 1 Arasında Olmalı.")                    
-    except:
-        print("Hata: Yalnızca Sayı Yazınız.")                          
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='') 
+@bot.event
+async def on_ready():
+    print(f'{bot.user} çalışmaya hazır!')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hi!')
+
+bot.run('TOKEN')                       
