943c0536584d34c06df2b06f630d8c0034044c17
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-20 01:42:51 +0300

    Update and rename LOS.lua to LOSu.lua

--- LOS.lua
+++
@@ -1,8 +0,0 @@
-local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AlkaV3rm/UiLib/master/Lib.lua")))()
-                                    
-local w = library:CreateWindow("DraWatX LOS")
-                                    
-local b = w:CreateFolder("Farming")
-                                    
-b:Toggle("City Auto Farm", function(bool)
-end)                                                                                              
