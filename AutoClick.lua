f87c9f335ba748ec3e6a29fd6a727c7712fb1793
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-20 00:06:55 +0300

    Update and rename AutoClick.lua to LOS.lua

--- AutoClick.lua
+++
@@ -1,7 +0,0 @@
-A_T = true;
-while (A_T == true)
-do
-    local args = {[1] = 100}
-    game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.ClickService.Click:FireServer(unpack(args))
-    wait()
-end
