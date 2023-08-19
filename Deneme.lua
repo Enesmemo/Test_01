e57485a44b906e85f7dca11b94518dbf02fa03e8
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-19 22:04:31 +0300

    Update Deneme.lua

--- Deneme.lua
+++ Deneme.lua
@@ -1,4 +1,5 @@
-while(true)
+A_T = true;
+while (A_T == true)
 do
     game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.7").knit.Services.ToolService.RE.onClick:FireServer()
     wait()
