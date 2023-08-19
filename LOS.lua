f0baa49638962ca9f7623656dc39a25e7230f9b7
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-20 00:20:32 +0300

    Update LOSu.lua

--- LOS.lua
+++ LOS.lua
@@ -8,5 +8,12 @@ do
     }
 
     game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+    local args2 = {
+        [1] = "collectOrb",
+        [2] = "Blue Orb",
+        [3] = "City"
+    }
+
+    game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args2))
     wait()
 end
