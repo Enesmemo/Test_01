11a9a785e0c1d241fd260044a10156a205ea7a71
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-20 00:09:43 +0300

    Update LOS.lua

--- LOS.lua
+++ LOS.lua
@@ -1,7 +1,12 @@
 A_F = true;
 while (A_F == true)
 do
-    local args = {[1] = "collectOrb", [2] = "Red Orb", [3] = "City"}
+    local args = {
+        [1] = "collectOrb",
+        [2] = "Red Orb",
+        [3] = "City"
+    }
+
     game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
     wait()
 end
