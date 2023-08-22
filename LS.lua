897e0da669f201fc4773438de77b4e25ae272b95
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-22 18:16:20 +0300

    Update LSu.lua

--- LS.lua
+++ LS.lua
@@ -1,42 +1,5 @@
-getgenv().A_C = false;
-getgenv().S = false;
-
-local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
-
-local w = library:CreateWindow("DraWatX LS")
-
-local a = w:CreateFolder("Auto")
-
-a:Toggle("Sell",function(bool)
-    getgenv().S = bool
-    if bool then
-        S()
-    end
-end)
-
-a:Toggle("Click",function(bool)
-    getgenv().A_C = bool
-    if bool then
-        A_C()
-    end
-end)
-
-function S()
-    spawn(function()
-        while (getgenv().S == true)
-        do
-            game:GetService("ReplicatedStorage").RemoteEvent:FireServer({"SellMuscle"})
-            wait()
-        end
-    end)
-end
-
-function A_C()
-    spawn(function()
-        while (getgenv().A_C == true)
-        do
-            game:GetService("ReplicatedStorage").RemoteEvent:FireServer({"GainMuscle"})
-            wait()
-        end
-    end)
+if game.PlaceId == 3652625463 then
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+    OrionLib:Init()
 end
