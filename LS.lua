ef736a4cbd8a1ad71de4c81a86da770a3492e5f0
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-22 19:35:38 +0300

    Update LS.lua

--- LS.lua
+++ LS.lua
@@ -3,7 +3,7 @@ if game.PlaceId == 3652625463 then
     local Window = OrionLib:MakeWindow({Name = "DraWatX LS", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
     local AutoTab = Window:MakeTab({
-        Name = "Click",
+        Name = "Auto",
         Icon = "rbxassetid://4483345998",
         PremiumOnly = false
     })
