e484d513f16069c6a49bd760e63ad09c7bbe329b
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-22 16:34:54 +0300

    Update LS.lua

--- LS.lua
+++ LS.lua
@@ -1,2 +1,7 @@
 getgenv().A_C = false;
 
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+
+local w = library:CreateWindow("DraWatX LS")
+
+local a = w:CreateFolder("Auto")
