5dbcd4ef87c9499437ce8c1b24ea6d0312842738
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-22 19:13:05 +0300

    Update and rename LS.lua to LSu.lua

--- LSu.lua
+++ LSu.lua
@@ -1,4 +1,10 @@
 if game.PlaceId == 3652625463 then
     local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
     local Window = OrionLib:MakeWindow({Name = "DraWatX LS", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+    local AutoTab = Window:MakeTab({
+        Name = "Click",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
 end
