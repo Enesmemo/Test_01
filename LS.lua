4f6ec27c556426595df8fbd4034ceac30f2497e7
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-22 19:40:13 +0300

    Update LS.lua

--- LS.lua
+++ LS.lua
@@ -1,4 +1,10 @@
 if game.PlaceId == 3652625463 then
+
+    getgenv().S = false;
+
+    function S()
+        
+    
     local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
     local Window = OrionLib:MakeWindow({Name = "DraWatX LS", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
@@ -7,4 +13,13 @@ if game.PlaceId == 3652625463 then
         Icon = "rbxassetid://4483345998",
         PremiumOnly = false
     })
+    
+    AutoTab:AddToggle({
+        Name = "Sell",
+        Callback = function(Value)
+            getgenv().S = Value
+            S()
+        end
+    })
 end
+OrionLib:Init()
