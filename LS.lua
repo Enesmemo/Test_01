413883a7a8b3e343c665d7e7172b2cc99b2c0688
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-22 20:19:24 +0300

    Update LS.lua

--- LS.lua
+++ LS.lua
@@ -16,7 +16,7 @@ if game.PlaceId == 3652625463 then
         spawn(function()
             while (getgenv().C == true) do
                 game:GetService("ReplicatedStorage").RemoteEvent:FireServer({"GainMuscle"})
-                wait(.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001)
+                wait(.00000000000000000000000000000000000000000000000000000000000000000001)
             end
         end)
     end
