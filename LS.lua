dd638d6c6b75c48e3a51ed9818478415670ed0b0
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-22 18:19:38 +0300

    Update and rename LS.lua to LSu.lua

--- LS.lua
+++
@@ -1,5 +0,0 @@
-if game.PlaceId == 3652625463 then
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-    local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-    OrionLib:Init()
-end
---
+++ LSu.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 3652625463 then
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "DraWatX Lefting Simulation", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+end
