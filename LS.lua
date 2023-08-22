7c581435b1fa9ea8c4ed987df9c961ba2aad43bd
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-22 17:43:09 +0300

    Update LS.lua

--- LS.lua
+++ LS.lua
@@ -1,4 +1,5 @@
 getgenv().A_C = false;
+getgenv().S = false;
 
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
 
@@ -6,12 +7,29 @@ local w = library:CreateWindow("DraWatX LS")
 
 local a = w:CreateFolder("Auto")
 
+a:Toggle("Sell",function(bool)
+    getgenv().S = bool
+    if bool then
+        S()
+    end
+end)
+
 a:Toggle("Click",function(bool)
     getgenv().A_C = bool
     if bool then
         A_C()
     end
 end)
+
+function S()
+    spawn(function()
+        while (getgenv().S == true)
+        do
+            game:GetService("ReplicatedStorage").RemoteEvent:FireServer({"SellMuscle"})
+            wait()
+        end
+    end)
+end
 
 function A_C()
     spawn(function()
