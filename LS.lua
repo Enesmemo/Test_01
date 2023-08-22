4043e095be5330efb8cb3184a54448933fa3fb53
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-22 22:21:20 +0300

    Update LS.lua

--- LS.lua
+++ LS.lua
@@ -1,3 +1,12 @@
 if game.PlaceId == 3652625463 then
 
+getgenv().C = false;
+getgenv().S = false;
+
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+
+local w = library:CreateWindow("DraWatX LOS")
+
+local a = w:CreateFolder("Auto")
+
 end
