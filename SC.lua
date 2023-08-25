ea7dd007d1ca191a9a2d3075a082bc3ad0b1a46d
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-25 07:43:11 +0300

    Update SC.lua

--- SC.lua
+++ SC.lua
@@ -1,5 +1,20 @@
 getgenv().F_C = true;
-while getgenv().F_C == true do
-  game:GetService("ReplicatedStorage").Packages.Knit.Services.ClickService.RF.Click:InvokeServer()
-  wait()
+
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+
+local w = library:CreateWindow("DraWatX LOS")
+
+local a = w:CreateFolder("Auto")
+
+local b = w:CreateFolder("Settings")
+
+b:DestroyGui()
+
+function A_C()
+  spawn(function)
+    while getgenv().F_C == true do  
+      game:GetService("ReplicatedStorage").Packages.Knit.Services.ClickService.RF.Click:InvokeServer()
+      wait()
+    end
+  end)
 end
