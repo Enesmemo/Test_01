8cee31289821823404a5f69b50d767b51640a0f4
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-26 07:26:03 +0300

    Update RPGC.lua

--- RPGC.lua
+++ RPGC.lua
@@ -1,5 +1,3 @@
-if game.PlaceId == 9031522337 then
-
 getgenv().A_C = false;  
 
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
@@ -36,5 +34,4 @@ function A_C()
             wait()
         end
     end)
-end
 end
