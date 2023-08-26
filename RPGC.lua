156e417d0c128efd5328540ee96d0f32d6f28ba0
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-26 06:49:03 +0300

    Create RPGC.lua

---
+++ RPGC.lua
@@ -0,0 +1,15 @@
+if game:PlaceId == 9031522337 then
+
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+
+local w = library:CreateWindow("DraWatX RPGC")
+
+local b = w:CreateFolder("Quest")
+
+b:Button("Menu Open",function()
+game:GetService("ReplicatedStorage").Remotes.ClientEVENT:Fire("hideMainExcept", "ZoneQuest")
+game:GetService("ReplicatedStorage").Remotes.ClientEVENT:Fire("updateQuests","Home")
+game:GetService("Players").LocalPlayer.PlayerScripts.LocalScript.Modules.QuestEvent:Fire("update", "Home")
+game:GetService("ReplicatedStorage").Remotes.actionFunctions:InvokeServer("getZoneQuests", "Home")
+end)
+end
