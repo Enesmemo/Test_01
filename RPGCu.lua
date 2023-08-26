5c26fbf6c12981e52af23a01a8b50ff856464f85
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-26 17:24:32 +0300

    Update and rename RPGC.lua to RPGCu.lua

--- RPGCu.lua
+++ RPGCu.lua
@@ -20,7 +20,7 @@ a:Toggle("Click",function(bool)
     end
 end)
 
-b:Dropdown("None",{"Shop","Quest"},true,function(value)
+b:Dropdown("None",{"Quest","Shop"},true,function(value)
     MValue = value
 end)
 
@@ -33,7 +33,7 @@ b:Button("Open",function()
     end
 end)
 
-c:Destroy()
+c:DestroyGui()
   
 function A_C()
     spawn(function()
