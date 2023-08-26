61891d5a2ea6a8abdd2b008c55c990030e234c5e
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-26 16:40:12 +0300

    Update and rename RPGC.lua to RPGCu.lua

--- RPGCu.lua
+++ RPGCu.lua
@@ -1,6 +1,5 @@
 if game.PlaceId == 9031522337 then
 
-mvalue
 getgenv().A_C = false;
 
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
@@ -20,17 +19,15 @@ a:Toggle("Click",function(bool)
     end
 end)
 
-b:Dropdown("None",{"Quest","B","C"},true,function(valuem)
-    mvalue = valuem
+b:Dropdown("None",{"Quest","B","C"},true,function(MValue)
+      print(MValue)
 end)
 
 b:Button("Open",function()
-    if  string.find(mvalue, "Quest") then
-        game:GetService("ReplicatedStorage").Remotes.ClientEVENT:Fire("hideMainExcept", "ZoneQuest")
-        game:GetService("ReplicatedStorage").Remotes.ClientEVENT:Fire("updateQuests","Home")
-        game:GetService("Players").LocalPlayer.PlayerScripts.LocalScript.Modules.QuestEvent:Fire("update", "Home")
-        game:GetService("ReplicatedStorage").Remotes.actionFunctions:InvokeServer("getZoneQuests", "Home")
-    end
+    game:GetService("ReplicatedStorage").Remotes.ClientEVENT:Fire("hideMainExcept", "ZoneQuest")
+    game:GetService("ReplicatedStorage").Remotes.ClientEVENT:Fire("updateQuests","Home")
+    game:GetService("Players").LocalPlayer.PlayerScripts.LocalScript.Modules.QuestEvent:Fire("update", "Home")
+    game:GetService("ReplicatedStorage").Remotes.actionFunctions:InvokeServer("getZoneQuests", "Home")
 end)
 
 f:DestroyGui()
