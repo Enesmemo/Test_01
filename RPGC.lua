c8c4599399a62d201573a5ae10a569194399858a
DraWatX <122899281+DraWatX@users.noreply.github.com>
2023-08-26 21:15:04 +0300

    Update RPGC.lua

--- RPGC.lua
+++ RPGC.lua
@@ -3,12 +3,48 @@ if game.PlaceId == 9031522337 then
     MValue = ""
     getgenv().A_C = false;
 
-    if string.find(MValue, "Quest") then
-        game:GetService("ReplicatedStorage").Remotes.ClientEVENT:Fire("hideMainExcept", "ZoneQuest")
-        game:GetService("ReplicatedStorage").Remotes.ClientEVENT:Fire("updateQuests","Home")
-        game:GetService("Players").LocalPlayer.PlayerScripts.LocalScript.Modules.QuestEvent:Fire("update", "Home")
-        game:GetService("ReplicatedStorage").Remotes.actionFunctions:InvokeServer("getZoneQuests", "Home")
-    end
+    local ATab = Window:MakeTab({
+	    Name = "Auto",
+	    Icon = "rbxassetid://4483345998",
+	    PremiumOnly = false
+    })
+
+    local MTab = Window:MakeTab({
+	    Name = "Menus",
+	    Icon = "rbxassetid://4483345998",
+	    PremiumOnly = false
+    })
+
+
+    ATab:AddToggle({
+	    Name = "Click",
+	    Default = false,
+	    Callback = function(Value)
+		    getgenv().A_C = Value
+            A_C()
+	    end    
+    })
+
+    MTab:AddDropdown({
+	    Name = "Dropdown",
+	    Default = "None",
+	    Options = {"Shop", "Quest"},
+	    Callback = function(Value)
+	        MValue = Value
+	    end    
+    })
+
+    MTab:AddButton({
+	    Name = "Open",
+	    Callback = function()
+            if string.find(MValue, "Quest") then
+                game:GetService("ReplicatedStorage").Remotes.ClientEVENT:Fire("hideMainExcept", "ZoneQuest")
+                game:GetService("ReplicatedStorage").Remotes.ClientEVENT:Fire("updateQuests","Home")
+                game:GetService("Players").LocalPlayer.PlayerScripts.LocalScript.Modules.QuestEvent:Fire("update", "Home")
+                game:GetService("ReplicatedStorage").Remotes.actionFunctions:InvokeServer("getZoneQuests", "Home")
+            end
+  	    end    
+    })
 
     function A_C()
         spawn(function()
